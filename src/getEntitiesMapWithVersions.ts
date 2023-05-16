a454320d789aea63ef5ad50c978feaa9e1479be0
Kostiantyn Popovych <kostiantyn.popovych.exp@gmail.com>
2023-05-16 14:24:13 -0700

    Remove async preload method to make the result of the execution predictable (#7)

--- src/getEntitiesMapWithVersions.ts
+++ src/getEntitiesMapWithVersions.ts
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import log from "fancy-log";
 import { wrappers } from "./utils";
 
-function getEntitiesMapWithVersionsSync(entities: Entity[], entitiesMap: Map): MapWithVersions {
+function getEntitiesMapWithVersions(entities: Entity[], entitiesMap: Map): MapWithVersions {
   const mapWithVersions: MapWithVersions = {};
   for (const entity of entities) {
     const name: Name = Array.isArray(entity) ? entity[0] : entity;
@@ -33,37 +33,4 @@ function getEntitiesMapWithVersionsSync(entities: Entity[], entitiesMap: Map): M
   return mapWithVersions;
 }
 
-async function getEntitiesMapWithVersionsAsync(
-  entities: Entity[],
-  entitiesMap: Map
-): Promise<MapWithVersions> {
-  const mapWithVersions: MapWithVersions = {};
-  for (const entity of entities) {
-    const name: Name = Array.isArray(entity) ? entity[0] : entity;
-    try {
-      const { nameInRegistry } = entitiesMap[name];
-      mapWithVersions[name] = {
-        nameInRegistry,
-        desiredVersion: Array.isArray(entity) ? entity[1] : "latest",
-      };
-      const currentDir = process.cwd();
-      const packagePath = resolve(currentDir, "node_modules", nameInRegistry);
-      const packageIsInstalled = await wrappers.asyncExists(packagePath);
-      if (packageIsInstalled) {
-        const { version } = require(packagePath + "/package.json");
-        mapWithVersions[name].currentVersion = version;
-        if (mapWithVersions[name].desiredVersion === "latest") {
-          const command = `npm view ${nameInRegistry} version`;
-          const remoteVersion = (await wrappers.asyncExec(command)).stdout;
-          mapWithVersions[name].desiredVersion = remoteVersion.trim();
-        }
-      }
-    } catch (e) {
-      delete mapWithVersions[name];
-      log.error(`Plugin ${name} is not handled yet.`);
-    }
-  }
-  return mapWithVersions;
-}
-
-export { getEntitiesMapWithVersionsSync, getEntitiesMapWithVersionsAsync };
+export default getEntitiesMapWithVersions;
